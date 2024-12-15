7a91c1df5924533cc511176857c9a3a92cbecf3c
alena-ctrl <63353663+alena-ctrl@users.noreply.github.com>
2024-12-15 21:58:59 +0100

    3

--- js/script.js
+++ js/script.js
@@ -9,13 +9,14 @@ function init() {
   const isMobile = window.outerWidth < 740;
   if (isMobile) {
     gsap.set("#frohe-block", { height: 0 });
+    document.querySelector("#frohe-block").style.margin = "0 10px";
   }
   tl.from("#first-page", { ease: "linear", autoAlpha: 0 })
     .from("#snow_1 > *", { stagger: 0.02 })
     .from("#right-circle > *", { stagger: 0.05 }, "<")
     .from("#snow_2 > g", { x: -150, stagger: 0.05 }, "-=1")
     .from("#right-balls > g", {
-      y: -220,
+      y: -120,
       duration: 0.7,
       stagger: { each: 0.1, from: "edges" },
       ease: "bounce",
@@ -36,8 +37,6 @@ function init() {
     .from(buttonOpen, { duration: 0.4 }, "<");
 }
 
-
-
 function initSecondPage() {
   firstPage.classList.add("hide");
   tl2
@@ -47,12 +46,9 @@ function initSecondPage() {
 
 buttonOpen.addEventListener("click", initSecondPage);
 
-
 window.addEventListener("load", function () {
   init();
 });
-
-
 
 
 
